92f8ff5b0313d42e0bdefd8262973d438fdc8d25
Elisandro Sams <elisandrosams@gmail.com>
2026-06-25 14:07:59 -0600

    added skeleton load state needs debugging

--- src/App.js
+++ src/App.js
@@ -23,6 +23,19 @@ function App() {
       : item
     ))
   }
+
+  function removeItem(item) {
+setCart(cart.filter(book => book.id !== item.id))
+}
+
+function numberOfItems() {
+  let counter = 0;
+  cart.forEach(item => {
+    counter += item.quantity
+  })
+  return counter
+}
+
   useEffect(() => {
     console.log(cart)
   }, [cart])
@@ -30,12 +43,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
+      <Nav numberOfItems={numberOfItems()} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" exact element={<Books books={books} />} />
           <Route path="/books/:id" element= {<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
-          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity}/>} />
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
         </Routes>
         <Footer />
       </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
+import EmptyCart from "../assets/empty_cart.svg"
+import { Link } from "react-router-dom";
 
-const Cart = ({ cart, changeQuantity }) => {
-    const [subtotal, setSubtotal] = useState()
-    useEffect(() => {
-        let price = 0;
-        cart.forEach((item) => {
-            price += +((item.salePrice || item.originalPrice) * item.quantity).toFixed(2)
-        });
-        setSubtotal(price)
-    }, [cart])
-
-    
+const Cart = ({ cart, changeQuantity, removeItem }) => {
+  const [subtotal, setSubtotal] = useState(0);
+  useEffect(() => {
+    let price = 0;
+    cart.forEach((item) => {
+      price += +(
+        (item.salePrice || item.originalPrice) * item.quantity
+      ).toFixed(2);
+    });
+    setSubtotal(price);
+  }, [cart]);
 
   return (
     <div id="books__body">
@@ -43,7 +45,12 @@ const Cart = ({ cart, changeQuantity }) => {
                           <span className="cart__book--price">
                             ${(book.salePrice || book.originalPrice).toFixed(2)}
                           </span>
-                          <button className="cart__book--remove">Remove</button>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(book)}
+                          >
+                            Remove
+                          </button>
                         </div>
                       </div>
                       <div className="cart__quantity">
@@ -68,19 +75,29 @@ const Cart = ({ cart, changeQuantity }) => {
                   );
                 })}
               </div>
+              {
+                cart.length === 0 && (<div className="cart__empty">
+                <img src={EmptyCart} alt="" className="cart__empty--img" />
+                <h2>You don't have any books in your cart!</h2>
+                <Link to="/books">
+                <button className="btn">Browse books</button>
+                </Link>
+              </div>)
+              }
+              
             </div>
-            <div className="total">
+            {cart.length > 0 && (<div className="total">
               <div className="total__item total__sub-total">
                 <span>Subtotal</span>
-                <span>${subtotal}</span>
+                <span>${subtotal.toFixed(2)}</span>
               </div>
               <div className="total__item total__tax">
-                <span>Tax</span>
+                <span>Sales tax</span>
                 <span>${(subtotal * 0.08).toFixed(2)}</span>
               </div>
               <div className="total__item total__price">
                 <span>Total</span>
-                <span>${(subtotal + (subtotal * 0.08)).toFixed(2)}</span>
+                <span>${(subtotal + subtotal * 0.08).toFixed(2)}</span>
               </div>
               <button
                 className="btn btn__checkout no-cursor"
@@ -88,7 +105,7 @@ const Cart = ({ cart, changeQuantity }) => {
               >
                 Proceed to checkout
               </button>
-            </div>
+            </div>)}
           </div>
         </div>
       </main>
